1e3d42ef0e5b880c8c338a88e0d246a6004f5400
Rakinne Foote <rakinnefoote@Rakinnes-MBP.lan>
2021-12-28 01:13:42 -0800

    tinker with posting document to collection

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const player = {
+    "firstName": "Precious",
+    "lastName": "Achiuwa",
+    "temporaryDisplayName": "Achiuwa, Precious",
+    "personId": "1630173",
+    "teamId": "1610612761",
+    "jersey": "5",
+    "isActive": true,
+    "pos": "F",
+    "heightFeet": "6",
+    "heightInches": "8",
+    "heightMeters": "2.03",
+    "weightPounds": "225",
+    "weightKilograms": "102.1",
+    "dateOfBirthUTC": "1999-09-19",
+    "teamSitesOnly": {
+      "playerCode": "precious_achiuwa",
+      "posFull": "Forward",
+      "displayAffiliation": "Memphis/Nigeria",
+      "freeAgentCode": ""
+    }
+}
+
 const couchbase = require('couchbase');
 
 async function main() {
@@ -5,9 +28,31 @@ async function main() {
         username: 'Administrator',
         password: 'password'
     })
+
+    const bucket = cluster.bucket('player-data');
+    const raptors = bucket.scope('rosters').collection('raptors');
+
+    upsertPrecious001(player, raptors);
+
 }
+
+const upsertPrecious001 = async (doc, collection) => {
+    try {
+        const key = 'player_001';
+        const result = await collection.upsert(key, doc)
+        console.log('Result from Upsertion: ');
+        console.log(result)
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+
 
 main().catch((err) => {
     console.log('Error -- ', err)
     process.exit()
 }).then(process.exit);
+
+
+
